82fa044ac7dec7196c6c69414c22dcc1d49f74c1
NureniJamiu <nurenijamiu951@gmail.com>
2024-01-07 20:47:14 +0100

    change dashboard logo

---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,8 @@
+<div class="flex gap-1 items-end">
+    <img
+        alt="logo"
+        src="{{ asset('images/logo.jpg') }}"
+        class="w-10 h-10 rounded-full"
+    />
+    <p style="margin-bottom: 5px;">Amatech<span class="font-semibold">Lasu</span></p>
+</div>
