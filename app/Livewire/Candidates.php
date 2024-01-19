73e88bf4958aadfb0857882770c1608107b54891
NureniJamiu <nurenijamiu951@gmail.com>
2024-01-19 16:18:16 +0100

    feat: fetch all candidates and render in view

--- app/Livewire/Candidates.php
+++ app/Livewire/Candidates.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Candidate;
 use Livewire\Component;
 
 class Candidates extends Component
@@ -9,10 +10,17 @@ class Candidates extends Component
     public $candidates;
 
 
+   
+
     public function render()
     {
         return view('livewire.candidates');
     }
 
-    
+    public function mount()
+    {
+        // Fetch all candidates from the database
+        $this->candidates = Candidate::with('position')->get();
+    }
+
 }
