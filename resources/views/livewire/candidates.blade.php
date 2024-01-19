5e5f6b33916bda08e49e0c7e3a7ddd0cbad4e0f3
NureniJamiu <nurenijamiu951@gmail.com>
2024-01-19 08:22:25 +0100

    add candidate profile card

--- resources/views/livewire/candidates.blade.php
+++ resources/views/livewire/candidates.blade.php
@@ -1,4 +1,65 @@
-<div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
-    <h2 class="">[incomplete]</h2>
+<div class="grid grid-cols-2 gap-3" style="grid-template-columns: repeat(2, minmax(0, 1fr));">
+    <!-- Card start -->
+        <div class="max-w-sm bg-white dark:bg-gray-900 rounded-xl overflow-hidden shadow-lg">
+            <div class="border-b px-4 pb-6">
+                <div class="text-center my-4">
+                    <img class="h-32 w-32 rounded-full border-4 border-white dark:border-gray-800 mx-auto my-4"
+                        src="{{asset('images/avatarmale.webp')}}" alt="">
+                    <div class="py-2">
+                        <h3 class="font-bold text-2xl text-gray-800 dark:text-white mb-1">Nureni Jamiu O.</h3>
+                        <div class="inline-flex text-gray-700 dark:text-gray-200 items-center">
+                            <span class="text-sm text-gray-600 dark:text-gray-200">Position vying for: </span>
+                            President
+                        </div>
+                        <br>
+                        <div class="inline-flex text-gray-700 dark:text-gray-200 items-center">
+                            <span class="text-sm text-gray-600 dark:text-gray-200">Level: </span>
+                            300L
+                        </div>
+                    </div>
+                </div>
+                <div class="flex gap-2 px-2">
+                    <button
+                        class="flex-1 rounded-full bg-blue-600 dark:bg-blue-800 text-white dark:text-white antialiased font-bold hover:bg-green-800 dark:hover:bg-blue-900 px-4 py-2" style="background: #17B169;">
+                        View full profile
+                    </button>
+                </div>
+            </div>
+        </div>
+    <!-- Card end -->
+
+
+    <!-- Card start -->
+        <div class="max-w-sm bg-white dark:bg-gray-900 rounded-xl overflow-hidden shadow-lg">
+            <div class="border-b px-4 pb-6">
+                <div class="text-center my-4">
+                    <img class="h-32 w-32 rounded-full border-4 border-white dark:border-gray-800 mx-auto my-4"
+                        src="{{asset('images/avatar_hijab.webp')}}" alt="">
+                    <div class="py-2">
+                        <h3 class="font-bold text-2xl text-gray-800 dark:text-white mb-1">Aisha Jumuah</h3>
+                        <div class="inline-flex text-gray-700 dark:text-gray-200 items-center">
+                            <span class="text-sm text-gray-600 dark:text-gray-200">Position vying for: </span>
+                            Financial Sec.
+                        </div>
+                        <br>
+                        <div class="inline-flex text-gray-700 dark:text-gray-200 items-center">
+                            <span class="text-sm text-gray-600 dark:text-gray-200">Level: </span>
+                            200L
+                        </div>
+                    </div>
+                </div>
+                <div class="flex gap-2 px-2">
+                    <button
+                        class="flex-1 rounded-full bg-blue-600 dark:bg-blue-800 text-white dark:text-white antialiased font-bold hover:bg-green-800 dark:hover:bg-blue-900 px-4 py-2" style="background: #17B169;">
+                        View full profile
+                    </button>
+                    <!-- <button
+                        class="flex-1 rounded-full border-2 border-gray-400 dark:border-gray-700 font-semibold text-black dark:text-white px-4 py-2">
+                        Message
+                    </button> -->
+                </div>
+            </div>
+        </div>
+    <!-- Card end -->
+
 </div>
