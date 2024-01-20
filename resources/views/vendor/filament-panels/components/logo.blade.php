7ffb4796a12c90880107513f5dfba1a6e136b957
NureniJamiu <nurenijamiu951@gmail.com>
2024-01-20 07:48:56 +0100

    change logo name

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -4,5 +4,5 @@
         src="{{ asset('images/logo.jpg') }}"
         class="w-10 h-10 rounded-full"
     />
-    <p style="margin-bottom: 5px;">Amatech<span class="font-semibold">Lasu</span></p>
+    <p style="margin-bottom: 5px;">Amatech<span class="font-semibold">Elect</span></p>
 </div>
