8f96db8123de9c0c7424c99a8d3536efe3431405
NureniJamiu <nurenijamiu951@gmail.com>
2024-01-22 02:00:21 +0100

    fix typo

--- app/Livewire/VotingCenter.php
+++ app/Livewire/VotingCenter.php
@@ -14,7 +14,6 @@ use Filament\Notifications\Notification;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
-use Masmerise\Toaster\Toaster;
 
 class VotingCenter extends Component implements HasForms, HasActions
 {
@@ -46,6 +45,7 @@ class VotingCenter extends Component implements HasForms, HasActions
             ->requiresConfirmation()
             ->size('lg')
             ->action(function (array $arguments) {
+
                 $current_user = Auth::user();
 
                 $prev_vote = DB::table('votes')
@@ -55,7 +55,7 @@ class VotingCenter extends Component implements HasForms, HasActions
                 if ($prev_vote) {
 
                     Notification::make()
-                        ->title('Sorry, you\ve already voted a candidate for this position')
+                        ->title('Sorry, you\'ve already voted a candidate for this position')
                         ->danger()
                         ->send();
                     return;
@@ -74,58 +74,6 @@ class VotingCenter extends Component implements HasForms, HasActions
                     ->success()
                     ->send();
             });
-    }
-
-    public function handleClick($candidate_name, $candidate_position)
-    {
-
-        $current_user = Auth::user();
-
-        if (!$current_user) {
-            return;
-        }
-
-        // set loading variable
-        $this->loading = true;
-
-        // find vote where the voter_id equals current_user_id AND candidate_position equals candidate_position [args]
-        $prev_vote = DB::table('votes')
-            ->where('voter_id', $current_user->id)
-            ->where('candidate_position', $candidate_position)
-            ->first();
-        // if vote exists, return with a message "Sorry, you've already voted a candidate"
-        if ($prev_vote) {
-            // Show error message
-            // session()->flash('message', ["type" => "error", "content" => "Sorry, You've already voted a candidate for this position"]);
-
-            Toaster::success('Sorry, You\'ve already voted a candidate for this position!');
-            // dd("Sorry, can't perform this operation");
-
-            // Reset loading state
-            $this->loading = false;
-
-            return;
-        };
-
-        // if no vote exist, create a new vote.
-        $vote = new Vote;
-
-        $vote->voter_id = $current_user->id;
-        $vote->candidate_name = $candidate_name;
-        $vote->candidate_position = $candidate_position;
-        // $vote->poll_id = $position->poll->id;
-
-        $vote->save();
-        // dd("Voted successfully!");
-
-        // Reset loading state
-        $this->loading = false;
-
-        // Show success message
-        // session()->flash('message', ["type" => "success", "content" => "Voted successfully!"]);
-
-        Toaster::success('Voted Successfully!');
-
     }
 
     public function render()
